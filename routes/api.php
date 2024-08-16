61a62a6e6679d5222083c65e27128f8bd2fb3775
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-08-16 18:18:32 +0300

    ...

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,6 @@ Route::group(['prefix' => 'student/v1'], function() {
     Route::get('/teachers' , [TeacherController::class , 'index'] ); 
     Route::get('/teachers/{teacher}' , [TeacherController::class , 'show'] ); 
     Route::get('/courses' , [CourseController::class , 'index'] );
-    Route::get('/courses/{course}' , [CourseController::class , 'show'] );
     Route::get('/courses/{course}/lessons/{lesson}' , [LessonController::class , 'show'] );
 
 
@@ -62,6 +61,8 @@ Route::group(['prefix' => 'student/v1'], function() {
             Route::post('/logout' , [LogoutController::class , 'index'] ); 
             Route::patch('/profile' , [ProfileController::class , 'update'] ); 
             Route::delete('/profile' , [ProfileController::class , 'delete'] ); 
+    Route::get('/courses/{course}' , [CourseController::class , 'show'] );
+            
             Route::post('/courses/{course}/lessons/{lesson}/watched' , [LessonController::class , 'watched'] ); 
             Route::get('exams/{exam}' , [ExamController::class , 'show'] );
             Route::get('exams/{exam}/result' , [ExamController::class , 'result'] );
