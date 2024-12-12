891da6d48343bb358e962f11632f9b8db9869c05
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-12-12 19:06:05 +0200

    ..

---
+++ lang/ar/trash.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'courses' => 'الكورسات',
+  'lessons' => 'الدروس',
+  'show all trashed courses' => 'عرض كافه الكورسات المحذوفه',
+  'show all trashed lessons' => 'عرض كافه الدروس المحذوفه',
+  'show all trashed students' => 'عرض كافه الطليه المحذوفه',
+  'students' => 'الطلاب',
+  'students_courses' => 'طلبه الكورسات',
+  'trashed courses' => 'الكورسات المحذوفه',
+  'trashed lessons' => 'الدروس المحزوفه',
+  'trashed students' => 'الطلاب المحذوفه',
+);
---
+++ lang/en/trash.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'students' => 'students',
+  'courses' => 'courses',
+  'lessons' => 'lessons',
+  'students_courses' => 'students_courses',
+  'show all trashed lessons' => 'show all trashed lessons',
+  'trashed lessons' => 'trashed lessons',
+  'show all trashed students' => 'show all trashed students',
+  'trashed students' => 'trashed students',
+  'show all trashed courses' => 'show all trashed courses',
+  'trashed courses' => 'trashed courses',
+);
