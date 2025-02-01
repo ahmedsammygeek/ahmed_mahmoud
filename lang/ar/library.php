675ef68f7e4d010296f3f94b08b6ebded5c00104
ahmedsammygeek <ahmedsamigeek@gmail.com>
2025-02-01 18:05:19 +0200

    ...

---
+++ lang/ar/library.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'add' => 'إضافه',
+  'add new file' => 'إضافه ملف جديد',
+  'added_by' => 'تم الاضافه بواسطه',
+  'allowed_views_number' => 'عدد المشاهدات المسموح بها',
+  'assgin students' => 'اضافه اللاب',
+  'cancel' => 'الغاء',
+  'course' => 'الكورس',
+  'download_allowed_number' => 'عدد مرات التحميل المسموح بها',
+  'file added successfully' => 'تم اضافه الملف بنجاح',
+  'file updated successfully' => 'تم تعديل الملف بنجاح',
+  'files' => 'الملفات',
+  'force_water_mark' => 'اجبار العلامه المائيه',
+  'lesson' => 'الدرس',
+  'library' => 'المكتبه',
+  'name' => 'الاسم',
+  'options' => 'الخصائص',
+  'show all files' => 'عرض كافه الملفات',
+  'size' => 'الحجم',
+  'unit' => 'الوحده',
+  'video' => 'الفديو',
+);
---
+++ lang/en/library.php
@@ -0,0 +1,23 @@
+<?php
+
+return array (
+  'library' => 'library',
+  'add new file' => 'add new file',
+  'assgin students' => 'assgin students',
+  'show all files' => 'show all files',
+  'name' => 'name',
+  'size' => 'size',
+  'added_by' => 'added_by',
+  'course' => 'course',
+  'options' => 'options',
+  'unit' => 'unit',
+  'lesson' => 'lesson',
+  'video' => 'video',
+  'files' => 'files',
+  'download_allowed_number' => 'download allowed number',
+  'allowed_views_number' => 'allowed views number',
+  'force_water_mark' => 'force water mark',
+  'cancel' => 'cancel',
+  'file added successfully' => 'file added successfully',
+  'file updated successfully' => 'file updated successfully',
+);
