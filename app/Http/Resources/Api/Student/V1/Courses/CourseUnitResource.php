d3ef52a49de270e8e62672a137847aff1edce66f
ahmedsammygeek <ahmedsamigeek@gmail.com>
2025-03-23 01:48:59 +0200

    ..

--- app/Http/Resources/Api/Student/V1/Courses/CourseUnitResource.php
+++ app/Http/Resources/Api/Student/V1/Courses/CourseUnitResource.php
@@ -18,7 +18,7 @@ class CourseUnitResource extends JsonResource
         return [
             'id' => $this->id , 
             'title' => $this->title , 
-            'lesson_mins' => '20 min'  , 
+            'lesson_mins' => 20  , 
             'lesson_count' => $this->lessons()->count() ,  
         ];
     }
