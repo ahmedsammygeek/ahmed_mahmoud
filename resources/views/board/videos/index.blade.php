cfa1f701e8d4d75c66587edf157e7eb50f108036
ahmedsammygeek <ahmedsamigeek@gmail.com>
2025-05-26 19:50:22 +0300

    ..

---
+++ resources/views/board/videos/index.blade.php
@@ -0,0 +1,14 @@
+@extends('board.layout.master')
+
+@section('page_title' , 'عرض كافه الفديوهات' )
+
+@section('breadcrumb')
+<a href="{{ route('board.videos.index') }}" class="breadcrumb-item"> الفديوهات </a>
+
+<span class="breadcrumb-item active"> عرض كافه الفديوهات  </span>
+@endsection
+
+@section('page_content')
+@livewire('board.videos.list-all-videos') 
+@endsection
+
